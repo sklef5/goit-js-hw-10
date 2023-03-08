30d0da67d7265456af80380ccab2b58cf3887104
DVorortnoy <dimchik505@gmail.com>
2023-03-08 16:41:55 +0200

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,16 @@
 import Notiflix from 'notiflix';
 
-const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const FILTER_RESPONSE = new URLSearchParams({
-    fields: "name,capital,population,flags,languages"})
+  fields: 'name,capital,population,flags,languages',
+});
 // function responseInput  (name){
-export function fetchCountries(name){
-    return fetch(`${BASE_URL}${name}?${FILTER_RESPONSE}`)
-    .then(r=>{
-        if (!r.ok){
-            Notiflix.Notify.failure("Oops, there is no country with that name")
-            return;
-        }
-        return r.json();
-    })
- }
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?${FILTER_RESPONSE}`).then(r => {
+    if (!r.ok) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      return;
+    }
+    return r.json();
+  });
+}
